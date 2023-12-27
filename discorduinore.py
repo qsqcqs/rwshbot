b30e2bd098c59073e8639fca8b1e64dd2d6d06be
qsqcqs <118565709+qsqcqs@users.noreply.github.com>
2023-12-27 14:10:14 -0800

    Add files via upload
    completely reworked garbage

--- discorduinore.py
+++ discorduinore.py
@@ -1,73 +1,101 @@
-import discord
-from discord.ui import Select, View
 
-import os
-from discord.ext import commands
-from dotenv import load_dotenv
-import discord.ui
-import re
-load_dotenv()
-intents=discord.Intents.all()
-client=commands.Bot(intents=intents)
+
 slugcats = ['Survivor','Monk','Hunter','Artificer','Gourmand','Rivulet','Spearmaster','Saint','Sofanthiel','Nightcat']
 iterators = ['Five Pebbles','Looks to the Moon','Seven Red Suns','No Significant Harassment', 'Sliver of Straw', 'Unparalleled Innocence', 'Chasing Wind']
 flags = ['gayflag', 'lesbianflag', 'queerflag', 'straightflag']
 dumbshitlist=['Survivor','Monk','Hunter','Artificer','Gourmand','Rivulet','Spearmaster','Saint','Sofanthiel','Nightcat','Five Pebbles','Looks to the Moon','Seven Red Suns','No Significant Harassment', 'Sliver of Straw', 'Unparalleled Innocence', 'Chasing Wind']
+
+
+import os
+import discord
+from dotenv import load_dotenv
+import random
+import re
+from PIL import Image
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'Successfully logged into Discord as "{client.user}"\nAwaiting user input...')
     await client.change_presence(status=discord.Status.online, activity=discord.Game(name="Rain World: Downpour Dating Sim"))
 
-@client.slash_command()
-async def kill(ctx: discord.ApplicationContext,argx,argy):
-    if ctx.author.id == 625221549544177665:
-
-        await ctx.response.send_message("PID " + str(os.getpid()) + " killed.")
-        sys.exit()
-@client.slash_command()
-async def help(ctx: discord.ApplicationContext,argx,argy):
-    await ctx.response.send_message("\'/kill\'")
-    nickname = random.choice(['Sofanthiel', '???', 'Inv', 'Enot', 'Paincat'])
-    await message.guild.get_member(client.user.id).edit(nick=nickname.upper())
-@client.slash_command()
-async def inv(ctx: discord.ApplicationContext,argx,argy,argz):
-    nickname = random.choice(['Sofanthiel', '???', 'Inv', 'Enot', 'Paincat'])
-    await message.guild.get_member(client.user.id).edit(nick=nickname.upper())
-    if argx == "who":
-        while len(re.findall("\swho(?=[\'\s])",argy)) != 0: 
-            argx=re.sub('\swho(?=[\'\s])',dumbshitlist[random.randint(0,15)],'message.content',count=1)
-        await ctx.response.send_message(argx)
-    if argx == "www":
-        if random.randint(0,1)==1:
-            await ctx.response.send_message(f"In a fight betweeen {argy.lower()} and {arzz.lower()}. {argy.lower()} would win")
-        else:
-            await ctx.response.send_message(f"In a fight betweeen {argy.lower()} and {arzz.lower()}. {argz.lower()} would win")
-    if argx == "crackship":
-        if argy == "scug":
-            lis=slugcats
-        else:
-            if argy == "iterators":
-                lis=iterators
-            else:
-                return
-        if len(argz) == 0:
-            argz = flags[random.randint(0,3)]
-        else:
-            argz = (str(argz)+"flag")
+@client.event
+async def on_message(message):
+    k=str(message.content)
+    print(k)
+    print(k[0:4])
+    if k.find("inv!") != -1:
         
-        char1=lis[random.randint(0,len(lis-1))]
-        char2=lis[random.randint(0,len(lis-1))]
-        while char2==char1:
-            char2=lis[random.randint(0,len(lis-1))]
-        img1 = img1 = Image.open(f'1{char1.lower()}.png')
-        img2 = Image.open(f'2{char2.lower}.png')
-        img3 = Image.open(f'{argz}.png')
-        imageoutput = Image.open("output.png")
-        imageoutput.paste(img3,(0,0))
-        imageoutput.paste(img2, (0,0), img2)
-        imageoutput.paste(img1, (0, 0), img1)
-        imageoutput.save("output.png")
-        await ctx.response.send_message(f"{char1.lower()}x{char2.lower()}", file=discord.File('output.png'))
+        nickname = ['Sofanthiel', '???', 'Inv', 'Enot', 'Paincat'][random.randint(0,4)]
+        await message.guild.get_member(client.user.id).edit(nick=nickname)
+        k=k[4:]
+        print(k)
+        if k == "help":
+            print(dir(message))
+            await message.channel.send("uhh...like idk the commands")
+            return
+        if k[0:3] == "who":
+            k=k[4:]+" "
+            while len(re.findall("\s?who(?=[\b\'\s])",k)) != 0:
+ 
+                k=re.sub('\s?who(?=[\b\'\s])'," "+dumbshitlist[random.randint(0,15)],k,count=1)    
+            await message.channel.send(k)
+            
+            return
+        
+        if k[0:3] == "www":
+            k=k[4:]
+            s=k.find(" vs ")
+            
+            
+               
+
+            
+            if random.randint(0,1)==1:
+                await message.channel.send(f"In a fight betweeen {k[:s].lower()} and {k[s+4:].lower()}. {k[:s].lower()} would win")
+            else:
+                await message.channel.send(f"In a fight betweeen {k[:s].lower()} and {k[s+4:].lower()}. {k[s+4:].lower()} would win")
+        if k[0:9] == "crackship":
+            k=k[10:]
+            soi=random.randint(0,1)
+            flag=random.randint(0,3)
+            if k.find("scug")!=-1:
+                soi=0
+            if k.find("iter")!=-1:
+                soi=1
+            if k.find("straight")!=-1:
+                flag=3
+            if k.find("lesbian")!=-1:
+                flag=1
+            if k.find("gay")!=-1:
+                flag=0
+            if k.find("queer")!=-1:
+                flag=2
+            i=["gayflag","lesbianflag","queerflag","straightflag"][flag]
+            if soi==0:
+                lis=slugcats
+            if soi==1:
+                lis=iterators
+            char1=lis[random.randint(0,len(lis)-1)]
+            char2=lis[random.randint(0,len(lis)-1)]
+            while char2==char1:
+                char2=lis[random.randint(0,len(lis-1))]
+            print(f"1{char2.lower()}")
+            img1 = img1 = Image.open(f'1{char1.lower()}.png')
+            
+            img2 = Image.open(f'2{char2.lower()}.png')
+            img3 = Image.open(f'{i}.png')
+            imageoutput = Image.open("output.png")
+            imageoutput.paste(img3,(0,0))
+            imageoutput.paste(img2, (0,0), img2)
+            imageoutput.paste(img1, (0, 0), img1)
+            imageoutput.save("output.png")
+            await message.channel.send(f"{char1.lower()}x{char2.lower()}", file=discord.File('output.png'))
 
 
-client.run(os.environ.get('DISCORD_TOKEN'))
+client.run(TOKEN)
+
